e687a2ab2d4fc41970eeb8d60ae17fc80b03a545
arhangel2i <arhangel2i@mail.ru>
2022-02-07 15:56:22 +0300

    Add Lesson_01

---
+++ Lesson_01/lec.py
@@ -0,0 +1,19 @@
+#int, float, boolean, str, list,none
+from operator import truediv
+
+value = None
+a=123
+b=1.23
+#print(type(a))
+#print(type(b))
+value =12222
+#print(type(value))
+s='hello \'world\''
+#print(s) #вывод строки
+print(f"{a}-{b}-{s}")
+
+f=True
+#print(type(f))
+
+list = [1,2,3,'4',5]
+print(list)
