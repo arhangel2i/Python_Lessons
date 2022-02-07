94181d9ba9783d747b32e24d04ef658dbf9497e3
arhangel2i <arhangel2i@mail.ru>
2022-02-07 17:21:34 +0300

    add lesson

--- Lesson_01/lec.py
+++ Lesson_01/lec.py
@@ -10,10 +10,27 @@ value =12222
 #print(type(value))
 s='hello \'world\''
 #print(s) #вывод строки
-print(f"{a}-{b}-{s}")
+#print(f"{a}-{b}-{s}")
 
 f=True
 #print(type(f))
 
 list = [1,2,3,'4',5]
-print(list)
+#print(list)
+
+#print('Введите a')
+#a=int(input())
+#print('Введите b')
+#b=int(input())
+
+#print(f"{a} - {b} = {a+b}")
+
+a='123456789'
+print(a[2:4])
+
+def ft(x):
+    if x==1:
+        return True
+
+
+print(ft(1))
